dde684493b93564ec89e8cb21993a46fdc03bc91
SohaibAlmomani <almomanisohaib95@yahoo.com>
2022-06-09 06:05:18 +0300

    api-server

---
+++ src/models/index.js
@@ -0,0 +1,38 @@
+"use strict";
+require('dotenv').config();
+
+const POSTGRES_URI = process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.DATABASE_URL;
+const { Sequelize, DataTypes } = require("sequelize");
+
+const Food = require("./food");
+const Clothes = require("./clothes");
+const Collection = require('./collection-class');
+
+let sequelizeOptions = process.env.NODE_ENV === "production" ?
+    {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            },
+            native: true
+        }
+    } : {};
+let sequelize = new Sequelize(POSTGRES_URI, sequelizeOptions);
+
+const foodTable = Food(sequelize, DataTypes);
+const clothesTable = Clothes(sequelize, DataTypes);
+const foodCollection = new Collection(foodTable);
+const clothesCollection = new Collection(clothesTable);
+
+module.exports = {
+    db: sequelize,
+    // Before Collection class :
+    // Food: Food(sequelize, DataTypes),
+    // Clothes: Clothes(sequelize, DataTypes)
+    // After Collection class :
+    FoodTable: foodCollection,
+    ClothesTable: clothesCollection
+};
